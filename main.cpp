bc817c5bf1351bb2fe61171e3538bff11143d784
Danitim <danitim4@gmail.com>
2024-04-03 23:59:40 +0300

    Changed title font to Bangers, instead of Papyrus, because Papyrus was used in the Avatar movie and Ryan Gosling does not approved this

--- main.cpp
+++ main.cpp
@@ -2,17 +2,13 @@
 #include "qapplication.h"
 
 #include <QApplication>
-#include <QFontDatabase>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    // Загрузка шрифтов из ресурсов
-    int fontId = QFontDatabase::addApplicationFont(":/fonts/bangers.ttf");
-    QString bangersFont = QFontDatabase::applicationFontFamilies(fontId).at(0);
-
     MainWindow w;
     w.show();
+
     return a.exec();
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 #include <QLabel>
+#include <QFontDatabase>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -8,7 +9,10 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-
+    // Подгрузка шрифта для названия
+    int bangerstId = QFontDatabase::addApplicationFont(":/fonts/bangers.ttf");
+    QString bangersFamily = QFontDatabase::applicationFontFamilies(bangerstId).at(0);;
+    ui->title->setFont(QFont(bangersFamily, 128));
 }
 
 MainWindow::~MainWindow()
