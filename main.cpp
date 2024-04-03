cddc97943998cb2249c208290ca152738ce99b5f
Danitim <danitim4@gmail.com>
2024-04-03 23:47:07 +0300

    Added title font and added to resourses

--- main.cpp
+++ main.cpp
@@ -2,10 +2,16 @@
 #include "qapplication.h"
 
 #include <QApplication>
+#include <QFontDatabase>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    // Загрузка шрифтов из ресурсов
+    int fontId = QFontDatabase::addApplicationFont(":/fonts/bangers.ttf");
+    QString bangersFont = QFontDatabase::applicationFontFamilies(fontId).at(0);
+
     MainWindow w;
     w.show();
     return a.exec();
