9e304fc4f8a4436f40419844b2543bc682fa747c
Danitim <danitim4@gmail.com>
2024-04-04 11:48:47 +0300

    Changed icons colour and fixed russian text font issue

--- main.cpp
+++ main.cpp
@@ -8,11 +8,10 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
+    // Установка шрифта по умолчанию
     int fontId = QFontDatabase::addApplicationFont(":/fonts/nightmare.ttf");
     if (fontId != -1) {
-        // Получение названия шрифта
         QString fontFamily = QFontDatabase::applicationFontFamilies(fontId).at(0);
-        // Установка шрифта по умолчанию для приложения
         QApplication::setFont(QFont(fontFamily));
     } else {
         qDebug() << "Warning, failed to load font.";
