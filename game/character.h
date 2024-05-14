3561d6f1c66df824d64a2730b063c9d4dedf3902
Виталий Клименко <klimenko.ve@phystech.edu>
2024-05-14 16:30:29 +0300

    Added death conditions for char

--- game/character.h
+++ game/character.h
@@ -68,7 +68,7 @@ protected:
 
     int blockRadius = 25; // Default value, assigned dynamically in constructor
 
-    int health = 100;
+    int health = 300;
     short lookDirection = 1;
     bool isBlocking = false;
     qreal accelerationX = 2;
