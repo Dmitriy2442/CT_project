0e88a3cbd090ced03ba34649ad811abf9f342a1f
Danitim <danitim4@gmail.com>
2024-05-15 15:44:04 +0300

    Changed application font

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
     int fontId = QFontDatabase::addApplicationFont(":/fonts/nightmare.ttf");
     if (fontId != -1) {
         QString fontFamily = QFontDatabase::applicationFontFamilies(fontId).at(0);
-        QApplication::setFont(QFont(fontFamily));
+        QApplication::setFont(QFont("Bauhaus 93")); // Not using nightmare, bauhaus is more retrowave
     } else {
         qDebug() << "Warning, failed to load font.";
     }
